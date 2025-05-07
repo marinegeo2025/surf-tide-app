fee0bfe01953a9069bec3f43d79edec36e4b867c
marinegeo2025 <barnardalex@gmail.com>
2025-05-07 12:40:11 +0100

    Update Marine data

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify is not supported in Next.js 15+,
-  // so remove it.
 };
 
 module.exports = nextConfig;
