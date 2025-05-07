972c523e956891faa5f2da805edf4d43a6c00bc2
marinegeo2025 <barnardalex@gmail.com>
2025-05-07 11:19:24 +0100

    Update Marine data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <main style={{ padding: '2rem' }}>
-      <h1>Welcome to the Surf Tide App 🌊</h1>
+      <h1>Welcome to the Surf Tide App 2 🌊</h1>
       <p>Visit <a href="/tidechart/stornoway">/tidechart/stornoway</a> to see the tide chart.</p>
     </main>
   );
 }
+
