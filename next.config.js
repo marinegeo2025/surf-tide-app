1239f59b3768ade8bb51033adbc4a58937ab9617
marinegeo2025 <barnardalex@gmail.com>
2025-05-07 11:25:09 +0100

    Update Marine data

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-- swcMinify: true   // Remove this line
 };
 
 module.exports = nextConfig;
+
